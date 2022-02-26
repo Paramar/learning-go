506b0a078690ff3bb49efb973bf59eeedb57e80a
程子民 <chengzimin@sunline.cn>
2022-02-26 18:26:00 +0800

    模块二作业

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/learning-go
 
 go 1.17
 
+require github.com/golang/glog v1.0.0
--- src/homework-2/main.go
+++ src/homework-2/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -26,20 +25,19 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 
 func headerHandler(w http.ResponseWriter, r *http.Request) {
 
-	io.WriteString(w, "------接收客户端 request，并将 request 中带的 header 写入 response header:--------\n")
 	for k, v := range r.Header {
-		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+		w.Header().Set(k, v[0])
 	}
 
 	log.Println("--------读取当前系统的环境变量中的 VERSION 配置，并写入 response header:-------------------")
-	os.Setenv("VERSION","1.0")
+	os.Setenv("VERSION", "1.0")
 	envVersoin := os.Getenv("VERSION")
-	w.Header().Set("VERSION",envVersoin)
+	w.Header().Set("VERSION", envVersoin)
 
 	log.Println("--------Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出:-------------")
 	statusCode := http.StatusOK
 	w.WriteHeader(statusCode)
 	remoteAddr := r.RemoteAddr
-	log.Println("客户端 IP: ",remoteAddr,"\n HTTP 返回码: ",statusCode)
+	log.Println("客户端 IP: ", remoteAddr, "\n HTTP 返回码: ", statusCode)
 
 }
